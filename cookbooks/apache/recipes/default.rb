716f1d9c417fefae188235734d8f240f1926456c
punitha-92 <punitha.atluri92@gmail.com>
2017-06-01 00:04:50 +0000

    added node attibutes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,13 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+if node['platform_family'] == "rhel" 
+    package = "httpd"
+elsif node['platform_family'] == "debian"
+    package = "apache2"
+end
 
-package ‘apache2’ do
-    package_name ‘httpd’
+package 'apache' do
+    package_name package
     action :install
 end
 
-service ‘apache2’ do
-    service_name ‘httpd’
+service 'apache' do
+    service_name package
     action [:start, :enable]
 end
